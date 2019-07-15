ff69b1f59cc73ad70131984a0e1848e025233491
sundae <haiqiang.xu@hand-china.com>
2019-07-16 00:14:47 +0800

    完成用户中心界面

--- src/components/user-center/user-center.vue
+++ src/components/user-center/user-center.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="slide">
     <div class="user-center">
-      <div class="back">
+      <div class="back" @click="back">
         <i class="icon-back"></i>
       </div>
       <div class="switches-wrapper">
@@ -10,11 +10,28 @@
           :switches="switches"
           :currentIndex="currentIndex"></switches>
       </div>
-      <div ref="playBtn" class="play-btn">
+      <div ref="playBtn" class="play-btn" @click="random">
         <i class="icon-play"></i>
         <span class="text">随机播放全部</span>
       </div>
       <div class="list-wrapper" ref="listWrapper">
+        <scroll ref="favoriteList" class="list-scroll" v-if="currentIndex === 0" :data="favoriteList">
+          <div class="list-inner">
+            <song-list
+              @select="selectSong"
+              :songs="favoriteList"></song-list>
+          </div>
+        </scroll>
+        <scroll ref="playList" class="list-scroll" v-if="currentIndex === 1" :data="playHistory">
+          <div class="list-inner">
+            <song-list
+              @select="selectSong"
+              :songs="playHistory"></song-list>
+          </div>
+        </scroll>
+      </div>
+      <div class="no-result-wrapper" v-show="noResult">
+        <no-result :title="noResultDesc"></no-result>
       </div>
     </div>
   </transition>
@@ -22,9 +39,21 @@
 
 <script type="text/ecmascript-6">
 import Switches from 'base/switches/switches'
+import SongList from 'base/song-list/song-list'
+import Scroll from 'base/scroll/scroll'
+import Song from 'common/js/song'
+import TopTip from 'base/top-tip/top-tip'
+import {mapGetters, mapActions} from 'vuex'
+import {playlistMixin} from 'common/js/mixin'
+import NoResult from 'base/no-result/no-result'
 export default {
+  mixins: [playlistMixin],
   components: {
-    Switches
+    Switches,
+    SongList,
+    Scroll,
+    TopTip,
+    NoResult
   },
   data() {
     return {
@@ -35,10 +64,55 @@ export default {
       ]
     }
   },
+  computed: {
+    noResult() {
+      if (this.currentIndex === 0) {
+        return !this.favoriteList.length
+      } else {
+        return !this.playHistory.length
+      }
+    },
+    noResultDesc() {
+      if (this.currentIndex === 0) {
+        return '暂无收藏歌曲'
+      } else {
+        return '你还没有听过歌曲'
+      }
+    },
+    ...mapGetters([
+      'favoriteList',
+      'playHistory'
+    ])
+  },
   methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+      this.$refs.listWrapper.style.bottom = bottom
+      this.$refs.favoriteList && this.$refs.favoriteList.refresh()
+      this.$refs.playList && this.$refs.playList.refresh()
+    },
+    back() {
+      this.$router.back()
+    },
     switchItem(index) {
       this.currentIndex = index
-    }
+    },
+    selectSong(song, index) {
+      if (index !== 0) {
+        // 需new 一下转song实例，因为缓存保存字符
+        this.insertSong(new Song(song))
+      }
+    },
+    random() {
+      let list = this.currentIndex === 0 ? this.favoriteList : this.playHistory
+      if (list.length === 0) return
+      list = list.map(item => new Song(item)) // 必须song实例，实例里面有必需的方法
+      this.randomPlay({list})
+    },
+    ...mapActions([
+      'insertSong',
+      'randomPlay'
+    ])
   }
 }
 </script>
